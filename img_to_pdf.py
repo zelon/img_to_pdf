aaf0a78243c59bffcc2dfd7ce3866cebe02f5170
KIM, JINWOOK <zelonion@gmail.com>
2018-03-09 23:27:51 +0900

    Remove unused codes and modify filename for spliting by limit size

--- img_to_pdf.py
+++ img_to_pdf.py
@@ -8,7 +8,6 @@ import os
 import sys
 
 def make_label_jpg(label, filename):
-    # -pointsize 100
     cmd = 'magick -background white -fill black -size 600x800 -gravity Center -font D2Coding label:"[{}]" "{}"'.format(label, filename)
     print(cmd)
     os.system(cmd)
@@ -19,12 +18,6 @@ def make_pdf_from_filenamelist(filename_list, output_filename):
     cmd = 'magick {} "{}.pdf"'.format(" ".join(quoted_filenames), output_filename)
     print(cmd)
     os.system(cmd)
-
-
-def convert(filename_list, output_filename):
-    label_filename = "label.jpg"
-    make_label_jpg(output_filename, label_filename)
-    make_pdf_from_filenamelist([label_filename] + filename_list, output_filename)
 
 
 def filter_filename(filelist):
@@ -71,35 +64,20 @@ def main(directory, output_filename, limit_size_mb):
 
     filenamelist_of_list = enumerate_by_total_size(filenames, limit_size_mb)
     for i in range(len(filenamelist_of_list)):
-        filename = "{}-{}".format(output_filename, i)
+        filename = "{} - ({} of {})".format(output_filename, i, len(filenamelist_of_list))
         if len(filenamelist_of_list) == 1:
             filename = "{}".format(output_filename)
-        convert(filenamelist_of_list[i], filename)
 
+        label_filename = "label.jpg"
+        make_label_jpg(filename, label_filename)
+        make_pdf_from_filenamelist([label_filename] + filenamelist_of_list[i], filename)
 
-def print_usage_and_exit():
-    exe_filename = os.path.basename(sys.argv[0])
-    print("Usage: {} directory output_filename [limit_size_mb]".format(exe_filename))
-    sys.exit(1)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Images files to pdf file')
     parser.add_argument('directory', help='directory containing image files')
     parser.add_argument('-o', '--output', help='output pdf file name without .pdf', default='')
     parser.add_argument('-l', '--limit_size_mb', help='size by megabyte to split', type=int, default=0)
-
-    """
-    if len(sys.argv) != 4 and len(sys.argv) != 3 and len(sys.argv) != 2:
-        print_usage_and_exit()
-
-    directory = sys.argv[1]
-    output_filename = directory
-    if len(sys.argv) >= 3:
-        output_filename = sys.argv[2]
-    limit_size_mb = 0
-    if len(sys.argv) >= 4:
-        limit_size_mb = int(sys.argv[3])
-    """
 
     args = parser.parse_args()
 
